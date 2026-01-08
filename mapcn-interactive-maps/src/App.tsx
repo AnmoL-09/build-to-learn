9f95fa8ea96ba30754c6e035ece555ff66e02f4f
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-01-08 22:32:48 +0530

    Fix broken submodule and add mapcn-interactive-maps as normal folder

---
+++ mapcn-interactive-maps/src/App.tsx
@@ -0,0 +1,12 @@
+import { BangloreMap } from './components/banglore-map'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <BangloreMap />
+    </div>
+  )
+}
+
+export default App
---
+++ mapcn-interactive-maps/src/components/banglore-map.tsx
@@ -0,0 +1,18 @@
+import { Card } from "./ui/card"
+import { Map, MapControls } from "./ui/map"
+
+export const BangloreMap = () => {
+  return (
+    <Card className="h-75 p-0 overflow-hidden">
+        <Map center={[77.594566, 12.971599]} zoom={9}>
+            <MapControls 
+            position="top-left"
+            showZoom
+            showCompass
+            showFullscreen
+            showLocate
+            />
+        </Map>
+    </Card>
+  );
+}
