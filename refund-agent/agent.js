cf0bc9a1b2d35b87feed78372477646cabd44b21
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-01-13 22:32:00 +0530

    add required modules and basic setup done

---
+++ refund-agent/agent.js
@@ -0,0 +1,24 @@
+import * as z from "zod";
+import { createAgent, tool } from "langchain";
+
+const getWeather = tool(
+  ({ city }) => `It's always sunny in ${city}!`,
+  {
+    name: "get_weather",
+    description: "Get the weather for a given city",
+    schema: z.object({
+      city: z.string(),
+    }),
+  },
+);
+
+const agent = createAgent({
+  model: "claude-sonnet-4-5-20250929",
+  tools: [getWeather],
+});
+
+console.log(
+  await agent.invoke({
+    messages: [{ role: "user", content: "What's the weather in Tokyo?" }],
+  })
+);
