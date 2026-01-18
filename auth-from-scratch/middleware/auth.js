efefa49638936b367af9b3a91902ee91edddb018
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-01-18 17:02:18 +0530

    add authentication middleware

---
+++ auth-from-scratch/middleware/auth.js
@@ -0,0 +1,17 @@
+const Session = require("../models/Session");
+
+module.exports = async function auth(req, res, next) {
+  const sid = req.cookies.session_id;
+  if (!sid) return res.sendStatus(401);
+
+  const session = await Session.findOne({ session_id: sid });
+  if (!session) return res.sendStatus(401);
+
+  if (session.expires_at < new Date()) {
+    await session.deleteOne();
+    return res.sendStatus(401);
+  }
+
+  req.user_id = session.user_id;
+  next();
+};
