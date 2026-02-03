19328b7eff4a8eb6a227cb86f62f08d29ca51682
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-03 15:31:50 +0530

    add Dockerfile

--- node-app-github-actions-deploy/index.js
+++ node-app-github-actions-deploy/index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
   return res.json({ msg: "Hello form the server." });
+});
 
-  app.listen(PORT, () => {
-    console.log(`Server is up and running on PORT ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is up and running on PORT ${PORT}`);
 });
